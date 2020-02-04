fabb89bc674d1082b7beabaa04eafd95c9e552c7
danjovic <danjovic@gmail.com>
2020-02-04 09:41:25 -0300

    Upload files to server

---
+++ firmware/main.c
@@ -0,0 +1,346 @@
+/* 
+   Atari 5200 Joystick Port Emulator
+   This device was designed to allow the development of Atari 5200 controllers whithout having the console
+   
+   Daniel Jose Viana, Janeiro de 2020 - danjovic@hotmail.com
+
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                                         ///
+///                                    MODULOS E DEFINICOES EXTERNOS                                        ///
+///                                                                                                         ///
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <pic14regs.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                                         ///
+///                                    CONFIGURAÇÃO DO CHIP                                                 ///
+///                                                                                                         ///
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//#use delay(clock=8000000)
+//#fuses INTRC_IO, NOPROTECT, NOBROWNOUT, NOWDT, PUT 
+uint16_t __at _CONFIG configWord = _INTRC_OSC_NOCLKOUT & _CPD_OFF &  _CP_OFF & _WDT_OFF & _PWRTE_ON & _MCLRE_OFF; // watchdog off
+
+/*
+   PIC16F628A
+                  +--___--+                            
+        VREF/RA2 --|1    18|-- RA1/AN1  POT_Y                   
+ROW0 3       RA3 --|2    17|-- RA0/AN0  POT_X                  
+ROW1 2       RA4 --|3    16|-- RA7 ROW2 1                    
+BOT_BTN MCLR/RA5 --|4    15|-- RA6 ROW3 4                      
+             GND --|5    14|-- VCC                            
+CAV_CNTRL    RB0 --|6    13|-- RB7 LINE3 8                     
+    RXD   RX/RB1 --|7    12|-- RB6 LINE2 5       
+    TXD   TX/RB2 --|8    11|-- RB5 LINE1 6
+TOP_BTN      RB3 --|9    10|-- RB4 LINE0 7
+                   +-------+  
+*/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                                         ///
+///                                    DEFINITIONS AND CONSTANTS                                            ///
+///                                                                                                         ///
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+#define delay5us()        do { __asm__("nop\n nop\n nop\n nop\n nop"); } while (0)
+
+static uint8_t rows[4];
+static uint8_t hline = 0; // 
+static uint8_t potx=0,poty=0;
+ 
+ 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                                         ///
+///                                        FUNCTION PROtOTYPES                                              ///
+///                                                                                                         ///
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void _putc (uint8_t c);
+void _puts (char *ptr);
+
+void measurePotentimeters(void);
+void scanKeyboard(void);
+void printResults(void);
+void _puts (char *ptr);
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                                         ///
+///                                          MAIN PROGRAM                                                   ///
+///                                                                                                         ///
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void main (void)
+{
+
+//
+// SETUP
+//  
+
+// Setup comparators
+CMCON = (_CM1 | _CM0); // CM<2:0> = 011  Two Common Reference Comparators
+
+/* Voltage reference, ViH min = 1.9V, ViH max = 2.6V, average ViH = 2.25V
+
+VRR=1        VDD=5V          +----VREF----+
+VR3  VR2  VR1  VR0  VR[3..0] VRR=1    VRR=0
+ 0    0    0    0       0     1,25     0,00
+ 0    0    0    1       1     1,41     0,21
+ 0    0    1    0       2     1,56     0,42
+ 0    0    1    1       3     1,72     0,63
+ 0    1    0    0       4     1,88     0,83
+ 0    1    0    1       5     2,03     1,04
+ 0    1    1    0       6     2,19     1,25
+ 0    1    1    1       7     2,34     1,46
+ 1    0    0    0       8     2,50     1,67
+ 1    0    0    1       9     2,66     1,88
+ 1    0    1    0       10    2,81     2,08
+ 1    0    1    1       11    2,97     2,29 <--
+ 1    1    0    0       12    3,13     2,50
+ 1    1    0    1       13    3,28     2,71
+ 1    1    1    0       14    3,44     2,92
+ 1    1    1    1       15    3,59     3,13  */
+VRCON = _VREN | _VRR | _VR3 | _VR1 | _VR0; // Vref = (11/24) * 5 = 2.29 Volts 
+
+
+// Setup I/O pins
+TRISA = 0xFF;  // All pins as inputs, initially
+TRISB = (uint8_t) ~(_TRISB3);  // Pin RB3 (CAV_CNTRL) as output
+
+// Turn on Pullups on port B
+NOT_RBPU=0;
+PORTB = (uint8_t) (_RB0 | _RB3 | _RB4 | _RB5 | _RB6 |_RB7 ); 
+
+// Setup Serial Port
+BRGH=1;
+TXEN=1;
+SYNC=0;
+SPEN=1;
+SPBRG = 25; // 9600 bps @ 4MHz
+
+
+// Setup Timer0 
+// reload time 192 for 15,75KHz
+__asm__("clrwdt");
+T0CS=0;   // Timer 0 clocked by internal CPU clock (4MHz)
+PSA=1;    // prescaler assigned to WDT (timer0 clocked at 1:1)
+TMR0 = 0; // Clear Timer 0
+
+
+
+//
+// Main loop
+//
+  for (;;) {  // TODO check RXbuffer for new commands
+	scanKeyboard();
+
+    TRISB0=1; RB0=0; // CAV OFF
+	measurePotentimeters(); 
+	
+	if ( (potx>220) && (poty>220) ) { // Normal joystick
+       _puts("[Joystick]");	   	
+	} else { // trackball connected
+       _puts("[TrackBall]");	   
+	}
+	
+    RB0=1; TRISB0=0; // CAV ON
+	measurePotentimeters(); 
+	
+	scanKeyboard();
+	printResults(); 
+  } // for  
+} // main
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                                         ///
+///                                               FUNCTIONS                                                 ///
+///                                                                                                         ///
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void measurePotentimeters(void) {
+	uint8_t j;
+	// Release capacitors to charge
+	TRISA0 = 1;
+	TRISA1 = 1;
+	
+
+	// timed loop, trimmed to 64us   7+9+9+34+5 = 64 cycles=> 64us @ 4MHz
+	for (hline=0;hline<228;hline++) {  // 7 cycles
+	  if (C1OUT==0) potx=hline; else __asm__("nop\n nop\n nop\n nop\n nop"); // 9 cycles
+	  if (C2OUT==0) poty=hline; else __asm__("nop\n nop\n nop\n nop\n nop"); // 9 cycles
+	   
+	  for (j=0;j<4;j++); // 2+8*4 = 34 cycles 
+	  __asm__("nop\n nop\n nop\n nop\n nop"); // 5 cycles
+
+	}
+	
+	// Hold capacitors on discharge
+	TRISA0=0; RA0=0;
+	TRISA1=1; RA1=0;
+}
+
+
+#define TRISROW0 TRISA3
+#define TRISROW1 TRISA4
+#define TRISROW2 TRISA6
+#define TRISROW3 TRISA7
+
+#define RROW0 RA3
+#define RROW1 RA4
+#define RROW2 RA6
+#define RROW3 RA7
+
+
+void scanKeyboard(void) {
+	uint8_t j;
+	
+//    8  5  6  7
+//	3 *  7  4  1  row0
+//	2 0  8  5  2  row1
+//	1 #  9  6  3  row2
+//	4    R  P  S  r0w3
+	
+	// Each row remains active by two horizontal lines (128us)
+	
+	// Select first line  4+4+50+4+23+42+1 = 128 cycles
+	TRISROW3 = 1; RROW3 = 1;          // 4 cycles
+	TRISROW0 = 0; RROW0 = 0;          // 4 cycles
+	for (j=0;j<6;j++);               // 2+8*6 = 50 cycles	
+	__asm__("nop\n nop\n nop\n nop"); // 4 cycles
+	rows[0] = (PORTB & 0xF0)>>4;     // 23 cycles
+	for (j=0;j<5;j++);               // 2+8*5 = 42 cycles
+	__asm__("nop");                   // 1 cycle
+	
+	
+	// Select 2nd line  4+4+50+4+23+42+1 = 128 cycles
+	TRISROW0 = 1; RROW0 = 1;          // 4 cycles
+	TRISROW1 = 0; RROW1 = 0;          // 4 cycles
+	for (j=0;j<6;j++);               // 2+8*6 = 50 cycles	
+	__asm__("nop\n nop\n nop\n nop"); // 4 cycles
+	rows[1] = (PORTB & 0xF0)>>4;     // 23 cycles
+	for (j=0;j<5;j++);               // 2+8*5 = 42 cycles
+	__asm__("nop");                   // 1 cycle	
+	
+	
+
+	// Select third line  4+4+50+4+23+42+1 = 128 cycles
+	TRISROW1 = 1; RROW1 = 1;          // 4 cycles
+	TRISROW2 = 0; RROW2 = 0;          // 4 cycles
+	for (j=0;j<6;j++);               // 2+8*6 = 50 cycles	
+	__asm__("nop\n nop\n nop\n nop"); // 4 cycles
+	rows[2] = (PORTB & 0xF0)>>4;     // 23 cycles
+	for (j=0;j<5;j++);               // 2+8*5 = 42 cycles
+	__asm__("nop");                   // 1 cycle
+	
+	// Select fourth line  4+4+50+4+23+42+1 = 128 cycles
+	TRISROW2 = 1; RROW2 = 1;          // 4 cycles
+	TRISROW3 = 0; RROW3 = 0;          // 4 cycles
+	for (j=0;j<6;j++);               // 2+8*6 = 50 cycles	
+	__asm__("nop\n nop\n nop\n nop"); // 4 cycles
+	rows[3] = (PORTB & 0xF0)>>4;     // 23 cycles
+	for (j=0;j<5;j++);               // 2+8*5 = 42 cycles
+	__asm__("nop");                   // 1 cycle
+	
+
+}
+
+	
+void _putc (uint8_t c) {
+	while (!TRMT); // wait for transmit buffer to be empty
+	TXREG = c;     // send character 
+}
+
+
+void _puts (char *ptr) {
+  while (*ptr) {
+    _putc (*ptr);
+	ptr++;
+  }
+}
+
+
+
+
+
+void printNumber( uint8_t n) {
+   uint8_t digit;
+   
+   digit='0';
+   while (n>100) {
+     digit++;
+	 n=n-100;
+   }
+   _putc(digit);
+   
+   digit='0';
+   while (n>10) {
+     digit++;
+	 n=n-10;
+   }
+   _putc(digit);
+   
+    digit='0';
+   while (n>1) {
+     digit++;
+	 n=n-1;
+   }
+   _putc(digit);
+}
+
+
+void printResults(void){
+  // print axes information
+  _puts("PotX:");
+  printNumber(potx);
+  _puts(" PotY:");
+  printNumber(poty);
+  
+  // print buttons
+  _puts(" Top:");
+  if (RB3==0) _putc('1'); else _putc('0');
+  _puts(" bot:");
+  if (RA5==0) _putc('1'); else _putc('0');
+  
+  // print Keys
+  _puts(" Keys:");
+  if (rows[1] & (1<<3)) _putc('0');
+  if (rows[0] & (1<<0)) _putc('1');
+  if (rows[1] & (1<<0)) _putc('2');
+  if (rows[2] & (1<<0)) _putc('3');
+  if (rows[0] & (1<<1)) _putc('4');
+  if (rows[1] & (1<<1)) _putc('5');
+  if (rows[2] & (1<<1)) _putc('6');
+  if (rows[0] & (1<<2)) _putc('7');
+  if (rows[1] & (1<<2)) _putc('8');
+  if (rows[2] & (1<<2)) _putc('9');
+  if (rows[0] & (1<<3)) _putc('*');
+  if (rows[2] & (1<<3)) _putc('#');
+
+  if (rows[3] & (1<<2)) _putc('R');
+  if (rows[3] & (1<<1)) _putc('P');
+  if (rows[3] & (1<<0)) _putc('S');
+
+  _puts("\n");  
+	
+
+}
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,55 @@
+# Atari 5200 Joystick Port Emulator
+
+This device was designed to allow the development of Atari 5200 controllers whithout having the console
+
+## BACKGROUND
+
+Atari 5200 systems joysticks provides analog directional controller, two fire buttons, 10 numeric keys , Asterisk, Hash, Pause, Start and Reset keys.
+
+The analog stick is read by measure the time a capacitor takes to charge, but contrary to contemporary systems it was done by hardware, specifically inside the POKEY chip.
+
+The keypad is a conventional matrix but the reading is also done in hardware by the POKEY chip, that activates a pair of analog multiplexers (external to POKEY). One of the muxes route a GND return to ground to activate the lines of the matrix and the other mux connects one of the four rows of the matrix to a input (KR1) of the POKEY chip. A binary counter drives both muxes (2 bits for each mux) and whenever a LOW state is detected on the KR1 input the binary value of the counter is latched and a IRQ is generated (to the 6502). Only one key can be detected at a time.
+
+Bottom fire button is also routed to KR2 input of POKEY Chip by a MUX (that is also driven by the binary counter). On the Atari Computers, this input is used to activate key modifiers (SHIFT, ALT, CONTROL). 
+
+The TOP fire button is read (on the 5200) by the GTIA chip (not by the POKEY chip). 
+
+Regarding interfacing, both Fire buttons are activated by a connection to Ground.
+
+Atari 5200 provides a dedicated pin to supply voltage to the potentiometers that in turn charge the internal timing capacitors to measure the position of each axis of the stick. Such voltage is internally adjustable in the range of 4.2 to 6 Volts (approximately) to compensate for tolerances of potentiometer values and threshold voltage of the POKEY chip inputs (ViH). The voltage adjustment requires opening the console and fiddle with a trimpot. Such voltage  (named CAV or Vpot) can be turned on/off and that provides a way to differentiate a trackball controller from a joystick. 
+
+Joysticks and paddles provides position by charging the timing capacitor with a fixed voltage through a variable resistance. Trackballs, on the other side provide a variable voltage across a fixed resistance, but unlike a traditional trackball (or a mouse) the 5200 trackballs do not provide relative movement, instead they provide a speed measurement. 
+The faster you turn the ball in one direction the closer the measurement gets to a maximum or a minimum of the equivalent axis, for instance turn the ball up quickly should provide a measurement for the 5200 equivalent to the analog stick full upward.  A steady trackball should provide the equivalent of a joystick in its center position. 
+
+Whenever the circuit of a trackball detects a fall in the CAV (Vpot) voltage it forces its outputs to a voltage close to 3 Volts (according with Atari technical documents). The timing measurements with such voltage will then be used by the game code as a reference for a "steady" trackball. Such measurements should be near the middle of the timing range.
+
+A joystick, on the other hand will never charge the capacitor with CAV (Vpot) is turned off, and the measurements will return a value above the maximum expected for a joystick. 
+
+The master clock for Pokey chip is the horizontal synchronization frequency. Timing is measured in terms of horizontal lines (64us each). Though the measurements can vary from 0 to 228 the the expected range for a controller is 10 to 190.
+
+## CIRCUIT
+
+The circuit is built around a PIC16F628A. This microcontroller was chosen over an AVR because of the dual analog comparator with adjustable voltage reference, that can be set to simulate different ViH threshold voltages of POKEY chip POT inputs. 
+Each pot input has a timing capacitor of 47nf, a series 1k8 resistor, and an extra 1nf capacitor just like the POT inputs of the POKEY chip in Atari 5200. 
+
+8 pins of the PIC are used to read the keypad. Each line is activated (held down) at a time and the rows are read one horizontal line time later (64us).
+
+Top and Bottom buttons are read and all the values are printed at the serial port.
+
+One of the pins of PORT B is connected directly to CAV.
+
+![Schematic](/doc/schematic.png)
+
+## SOFTWARE
+
+Software is written in C language and can be compiled using SDCC / GPUtils. 
+
+   
+
+
+
+
+
+
+ 
+
